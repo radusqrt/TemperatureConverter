3c6eba72b66816f14f4644cfbf7aab002d64946f
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2019-07-29 20:55:08 +0300

    Initial commit

---
+++ app/src/main/java/com/radusqrt/android/temperatureconverter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.radusqrt.android.temperatureconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText1);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+                if (editText.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float inputValue = Float.parseFloat(editText.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    editText.setText(String
+                            .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                } else {
+                    editText.setText(String
+                            .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+        }
+    }
+}
